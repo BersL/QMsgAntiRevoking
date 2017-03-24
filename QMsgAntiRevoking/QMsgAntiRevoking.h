147115c7849febd40b913b9c86759a635d1900c1
Bers <me@bersl.com>
2017-03-24 11:03:32 +0800

    Initial Commit

---
+++ QMsgAntiRevoking/QMsgAntiRevoking.h
@@ -0,0 +1,13 @@
+//
+//  QMsgAntiRevoking.h
+//  QMsgAntiRevoking
+//
+//  Created by Bers on 2017/3/24.
+//  Copyright © 2017年 Bers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QMsgAntiRevoking : NSObject
+
+@end
---
+++ QMsgAntiRevoking/QMsgAntiRevoking.m
@@ -0,0 +1,13 @@
+//
+//  QMsgAntiRevoking.m
+//  QMsgAntiRevoking
+//
+//  Created by Bers on 2017/3/24.
+//  Copyright © 2017年 Bers. All rights reserved.
+//
+
+#import "QMsgAntiRevoking.h"
+
+@implementation QMsgAntiRevoking
+
+@end
